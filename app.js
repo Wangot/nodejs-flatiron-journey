0578dacdd1fe73dc4522b86c06bc00baf94ad22e
Wangot <raymandeleano@gmail.com>
2013-07-30 17:39:24 +0800

    Add Resourceful and Restful support

--- app.js
+++ app.js
@@ -1,13 +1,48 @@
 var flatiron = require('flatiron'),
     path = require('path'),
+    restful     = require('restful'),
+    resourceful = require('resourceful'),
     app = flatiron.app;
 
-app.config.file({ file: path.join(__dirname, 'config', 'config.json') });
+var app = module.exports = flatiron.app;
+app.resources = app.resources || {};
 
-app.use(flatiron.plugins.http);
-
-app.router.get('/', function () {
-  this.res.json({ 'hello': 'world' })
+app.use(flatiron.plugins.http, {
+  headers: {
+    'x-powered-by': 'flatiron ' + flatiron.version
+  },
+  before: [test1],
+  after: []
 });
 
-app.start(3000);
+function test1(req, res) {
+    if(req.url != "/") console.log("We can put the validation here...");
+  res.emit('next');
+}
+
+var Creature = app.resources.Creature = resourceful.define('creature', function () {
+  this.restful = true;
+  //this.use('memory');
+  this.string('type');
+  this.string('description');
+});
+
+app.use(restful);
+
+app.on('init', function () {
+  var database = app.config.get('database') || {
+    host: 'localhost',
+    port: 5984
+  };
+  
+  database.database = database.database || 'test';
+
+  resourceful.use('couchdb', database);
+  resourceful.autoMigrate = true;
+});
+
+app.start(8080, function(){
+  console.log(app.router.routes)
+  console.log(' > http server started on port 8000');
+  console.log(' > visit: http://localhost:8000/ ');
+});
